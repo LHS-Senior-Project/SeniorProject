36e530ed8c5d5d93f859c586622bdb13656ba3e4
oojava <alexpetersonvb@gmail.com>
2015-03-01 16:58:43 -0500

    Just testing

--- core/src/com/S/P/SP2.java
+++ core/src/com/S/P/SP2.java
@@ -18,10 +18,17 @@ public class SP2 extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		update();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		
 		batch.end();
+//		derp.txt
+	}
+	
+	public void update(){
+		
 	}
 }
