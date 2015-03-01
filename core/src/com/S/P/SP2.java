ee372ca342d2a34b6a6b29b7453f376333c9e9f1
oojava <alexpetersonvb@gmail.com>
2015-03-01 18:16:51 -0500

    World works and camera is working too

--- core/src/com/S/P/SP2.java
+++ core/src/com/S/P/SP2.java
@@ -2,32 +2,164 @@ package com.S.P;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
-public class SP2 extends ApplicationAdapter {
+public class SP2 extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	Texture img;
+	OrthographicCamera camera;
+	World thisWorld;
+	
+	boolean up,down,left,right,zIn,zOut;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera(700f, 700f * Gdx.graphics.getHeight() / Gdx.graphics.getWidth());
+		camera.position.set(250f, 750f, 0f);
+		camera.update();
+		Gdx.input.setInputProcessor(this);
+		Gdx.input.setOnscreenKeyboardVisible(true);
+		thisWorld = new World(new Vector2(0.0f, -9.8f), 100, 80);
 	}
 
+	@Override
+	public void resize(int width, int height){
+		camera = new OrthographicCamera(700f, 700f * Gdx.graphics.getHeight() / Gdx.graphics.getWidth());
+		camera.update();
+	}
+	
 	@Override
 	public void render () {
 		update();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		for(Block[] blocks : thisWorld.blocks){
+			for(Block b : blocks){
+				batch.draw(b.blockTexture, b.position.x, b.position.y);
+			}
+		}
 		
 		batch.end();
 	}
 	
 	public void update(){
-		
+		if (up) {
+			camera.position.set(camera.position.x, camera.position.y + 16f, 0);
+		}
+		if (down) {
+			camera.position.set(camera.position.x, camera.position.y - 16f, 0);
+		}
+		if (left) {
+			camera.position.set(camera.position.x - 16f, camera.position.y, 0);
+		}
+		if (right) {
+			camera.position.set(camera.position.x + 16f, camera.position.y, 0);
+		}
+		if(zIn){
+			camera.viewportHeight = camera.viewportHeight - 16f;
+			camera.viewportWidth = camera.viewportWidth - 16f;
+		}
+		if(zOut){
+			camera.viewportHeight = camera.viewportHeight + 16f;
+			camera.viewportWidth = camera.viewportWidth + 16f;
+		}
+		camera.update();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		switch (keycode) {
+		case Keys.W:
+			up = true;
+			break;
+		case Keys.A:
+			left = true;
+			break;
+		case Keys.S:
+			down = true;
+			break;
+		case Keys.D:
+			right = true;
+			break;
+		case Keys.E:
+			zIn = true;
+			break;
+		case Keys.Q:
+			zOut = true;
+			break;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		switch (keycode) {
+		case Keys.W:
+			up = false;
+			break;
+		case Keys.A:
+			left = false;
+			break;
+		case Keys.S:
+			down = false;
+			break;
+		case Keys.D:
+			right = false;
+			break;
+		case Keys.E:
+			zIn = false;
+			break;
+		case Keys.Q:
+			zOut = false;
+			break;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
--- core/src/com/S/P/World.java
+++ core/src/com/S/P/World.java
@@ -15,21 +15,26 @@ public class World {
 	public World(){
 		setGravity(new Vector2(0, -9.8f));
 		blocks = new Block[50][50];
+		generateWorld();
 	}
 	
 	public World(Vector2 gravity, int WorldHeight, int WorldWidth){
 		setGravity(gravity);
-		//TODO check x and y
 		blocks = new Block[WorldHeight][WorldWidth];
+		generateWorld();
 	}
 
 	private void generateWorld(){
 		for(int x = 0; x < blocks.length; x++){
 			for(int y = 0; y < blocks[x].length; y++){
-				if(y < 53) blocks[x][y] = new Block(new Vector2(x, y), Block.DIRT);
-				if(y > 53) blocks[x][y] = new Block(new Vector2(x, y), Block.AIR);
+				blocks[x][y] = new Block(new Vector2(x * 16, y * 16), Block.DIRT);
+				if(y > 53) blocks[x][y] = new Block(new Vector2(x * 16, y * 16), Block.AIR);
 			}
 		}
+	}
+	
+	public Block[][] getBlocks(){
+		return blocks;
 	}
 	
 	private void setGravity(Vector2 gravity) {
