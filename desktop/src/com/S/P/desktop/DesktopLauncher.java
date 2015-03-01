0f7cb0a0bc4e1c83157892d58cd039c85ca44b19
oojava <alexpetersonvb@gmail.com>
2015-03-01 14:35:14 -0500

    init commit

---
+++ desktop/src/com/S/P/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.S.P.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.S.P.SP2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SP2(), config);
+	}
+}
---
+++ html/src/com/S/P/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.S.P.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.S.P.SP2;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SP2();
+        }
+}
